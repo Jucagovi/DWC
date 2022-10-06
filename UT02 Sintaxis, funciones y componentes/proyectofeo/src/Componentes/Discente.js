6f3d708f6260d810fff1faac2dd18dad251a2fb0
Jucagovi <jc.gomez@edu.gva.es>
2022-10-06 15:42:37 +0200

    Props

--- UT02 Sintaxis, funciones y componentes/proyectofeo/src/Componentes/Discente.js
+++ UT02 Sintaxis, funciones y componentes/proyectofeo/src/Componentes/Discente.js
@@ -7,21 +7,16 @@ function Discente(props) {
         {/* <p>Apellidos, Nombre</p>
         <p>Curso matrícula</p>
         <p>Módulos matrícula</p> */}
-        <p>
-          {props.datos.apellidos}, {props.datos.nombre}
-        </p>
-        <p>{props.datos.curso}</p>
-        <p>{props.datos.modulos}</p>
-        {/* <p>
+         {/* <p>
           {props.datos.apellidos}, {props.datos.nombre}
         </p>
         <p>{props.datos.curso}</p>
         <p>{props.datos.modulos}</p> */}
-        {/* <p>
+        <p>
           {props.apellidos}, {props.nombre}
         </p>
         <p>{props.curso}</p>
-        <p>{props.children}</p> */}
+        <p>{props.children}</p>
       </div>
     </React.Fragment>
   );
