d647ff0c3b6f5427ef95c7807b1d857733472966
Jucagovi <jc.gomez@edu.gva.es>
2022-11-30 12:42:13 +0100

    UT05 useEffect

--- codigo-react/src/Unidad05.js
+++ codigo-react/src/Unidad05.js
@@ -2,15 +2,18 @@ import React from "react";
 import Colores from "./Componentes/UT05/Colores.js";
 import Contador2 from "./Componentes/UT05/Contador2.js";
 import ContadorPlus from "./Componentes/UT05/ContadorPlus.js";
+import Planetas from "./Componentes/UT05/Planetas.js";
 
 function Unidad05() {
   return (
     <React.Fragment>
       {/* Control del ciclo de vida con useEffect. */}
-      <Contador2 />
+      {/* <Contador2 /> */}
       {/* <Colores /> */}
       {/* Dependecias en useEffect */}
       {/* <ContadorPlus /> */}
+      {/* Trabajando con peticiones externas */}
+      <Planetas />
     </React.Fragment>
   );
 }
