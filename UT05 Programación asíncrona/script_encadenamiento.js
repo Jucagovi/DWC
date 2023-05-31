c2bec0896d37548020c6036fdb306ee788905362
Jucagovi <jc.gomez@edu.gva.es>
2023-05-31 10:15:06 +0200

    Encadenamiento

---
+++ UT05 Programación asíncrona/script_encadenamiento.js
@@ -0,0 +1,26 @@
+"use strict";
+
+new Promise((resolver, rechazar) => {
+  setTimeout(() => {
+    return resolver(1);
+  }, 1000);
+})
+  .then((res_p1) => {
+    console.log(res_p1); // 1
+    return res_p1 + 1;
+  })
+  .then((res_p2) => {
+    console.log(res_p2);
+    return res_p2 + 1;
+  })
+  .then((res_p3) => {
+    console.log(res_p3);
+    return res_p3 + 1;
+  })
+  .catch(() => {
+    rechazar("Se ha producido un error.");
+  })
+  .finally(() => {
+    console.log("Proceso de encadenamiento de promesas terminado.");
+  });
+//comentario feo
